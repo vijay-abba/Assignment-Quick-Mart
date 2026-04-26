607b2f1676995f148c31e9ad9f6b79f15be9cffa
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-26 09:52:42 +0530

    get product type in operation

--- NewProductMenu.py
+++ NewProductMenu.py
@@ -4,51 +4,48 @@
 class ProductMenu:
 
     def __init__(self):
-        self.action_choice = self.run_until_valid(self.action_choice_validate_fn)
-
-        print("Action")
-        print(self.action_choice)
-        self.define_operation()
-
-    def action_choice_validate_fn(self):
-        action_list = ["1", "2", "3", "4", "5", "6"]
-        action_choice = input(
+        action_indexs = ["1", "2", "3", "4", "5", "6"]
+        action_message = (
             "\n1.Add  2.Update  3.Delete 4.Search  5.View All  6.Low Stock:\n"
         )
+
+        self.action_choice = self.run_until_valid(
+            self.action_choice_validate_fn, action_indexs, action_message
+        )
+
+        self.define_operation()
+
+    def action_choice_validate_fn(self, index_list, message):
+        action_list = index_list
+        action_choice = input(message)
         if action_choice in action_list:
             return action_choice
         else:
             print("\nInvalid choice, TRY AGAIN")
             return False
 
-    def run_until_valid(self, fn):
-        result = fn()
+    def run_until_valid(self, fn, index_list, message):
+        result = fn(index_list, message)
 
         if not result:
-            return self.run_until_valid(fn)
+            return self.run_until_valid(fn, index_list, message)
 
         return result
 
-
-    def product_choice_validate_fu(self):
-        action_list = ["1", "2", "3", "4"]
-        action_choice = input(
-            "\n1.General 2.Perishable 3.Electronic 4.Clothing:\n"
+    def prodoct_choice_fn(self):
+        product_index = ["1", "2", "3", "4"]
+        product_message = "\n1.General 2.Perishable 3.Electronic 4.Clothing:\n"
+        return self.run_until_valid(
+            self.action_choice_validate_fn, product_index, product_message
         )
-        if action_choice in action_list:
-            return action_choice
-        else:
-            print("\nInvalid choice, TRY AGAIN")
-            return False
-
-
 
     def define_operation(self):
         if self.action_choice == "1":
-            print("action selected 2 add")
+            product_type_choice = self.prodoct_choice_fn()
+            print(product_type_choice)
 
             # TASK
-            # 1 GET PRODUCT TYPE
+            # 1 GET PRODUCT TYPE Completed
             # 2 GET PRODUCT DETAILS
             pass
         elif self.action_choice == "2":
