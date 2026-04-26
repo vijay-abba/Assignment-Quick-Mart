d37d69b2939d8ca8cbffa60765fc600ab9f76640
Vijay <109028484+vijay-abba@users.noreply.github.com>
2026-04-26 07:26:12 +0530

    New Product class is completed

--- NewProduct.py
+++ NewProduct.py
@@ -9,78 +9,219 @@ class Product:
 
     def __init__(self):
         # connect to database / product txt
-        self.product_list = self.read_from_file()
-
+        self.product_list = ""
+        self.read_from_file()
 
     def read_from_file(self):
         with open(file_name, "r") as f:
             str_product_list = f.readline()
             product_list = json.loads(str_product_list)
-            return product_list
+            self.product_list = product_list
 
     def writ_to_file(self):
         str_product_list = json.dumps(self.product_list)
         with open(file_name, "w") as f:
             f.write(str_product_list)
+        self.read_from_file()
 
     def add(self, name, quantity):
+
         id = f"PRD-{len(self.product_list)+1:04d}"
-        new_product_obj = {"id": id, "name": name, "quantity": quantity}
+        new_product_obj = {"id": id, "name": name, "quantity": quantity, "type": "1"}
 
         self.product_list.append(new_product_obj)
         self.writ_to_file()
 
     def update(self, id, name, quantity):
-        new_product_obj = {"id": id, "name": name, "quantity": quantity}
+        
+
+        new_product_obj = {"id": id, "name": name, "quantity": quantity, "type": "1"}
         self.delete(id)
         self.product_list.append(new_product_obj)
         self.writ_to_file()
 
     def delete(self, id):
+
         self.product_list = list(
             filter(lambda item: item["id"] != id, self.product_list)
         )
         self.writ_to_file()
 
     def search(self, name):
-        filtered_list = list(filter(lambda x: x["name"].lower().startswith(name.lower()), self.product_list))
+        filtered_list = list(
+            filter(
+                lambda x: x["name"].lower().startswith(name.lower()), self.product_list
+            )
+        )
         print(filtered_list)
 
     def view_all(self):
+
         for i in self.product_list:
-            print(f'{i["id"]} {i["name"]} {i["quantity"]}')
+            # print(f'{i["id"]} {i["name"]} {i["quantity"]}')
+            print(i)
 
     def lowstock(self):
-        filtered_list = list(filter(lambda x : x["quantity"] < 5, self.product_list))
+        filtered_list = list(filter(lambda x: x["quantity"] < 5, self.product_list))
         print(filtered_list)
 
 
 class PerishableProduct(Product):
-    def __init__(self, id, name, quantity, expiry_date):
-        super().__init__(id, name, quantity)
-        self.expiry_date = expiry_date
+    def __init__(self):
+        super().__init__()
+
+    def add(self, name, quantity, expiry_date):
+
+        id = f"PRD-{len(self.product_list)+1:04d}"
+        new_product_obj = {
+            "id": id,
+            "name": name,
+            "quantity": quantity,
+            "type": "2",
+            "expiry_date": expiry_date,
+        }
+
+        self.product_list.append(new_product_obj)
+        self.writ_to_file()
+
+    def update(self, id, name, quantity, expiry_date):
+        
+
+        new_product_obj = {
+            "id": id,
+            "name": name,
+            "quantity": quantity,
+            "type": "2",
+            "expiry_date": expiry_date,
+        }
+        self.delete(id)
+        self.product_list.append(new_product_obj)
+        self.writ_to_file()
 
 
 class ElectronicProduct(Product):
-    def __init__(self, id, name, quantity, warranty):
-        super().__init__(id, name, quantity)
-        self.warranty = warranty
+    def __init__(self):
+        super().__init__()
+
+    def add(self, name, quantity, warranty):
+
+        id = f"PRD-{len(self.product_list)+1:04d}"
+        new_product_obj = {
+            "id": id,
+            "name": name,
+            "quantity": quantity,
+            "type": "3",
+            "warranty": warranty,
+        }
+
+        self.product_list.append(new_product_obj)
+        self.writ_to_file()
+
+    def update(self, id, name, quantity, warranty):
+        
+        new_product_obj = {
+            "id": id,
+            "name": name,
+            "quantity": quantity,
+            "type": "3",
+            "warranty": warranty,
+        }
+        self.delete(id)
+
+        self.product_list.append(new_product_obj)
+        self.writ_to_file()
 
 
 class ClothingProduct(Product):
-    def __init__(self, id, name, quantity, size, material):
-        super().__init__(id, name, quantity)
-        self.size = size
-        self.material = material
+    def __init__(self):
+        super().__init__()
+
+    def add(self, name, quantity, size, material):
+        id = f"PRD-{len(self.product_list)+1:04d}"
+        new_product_obj = {
+            "id": id,
+            "name": name,
+            "quantity": quantity,
+            "type": "4",
+            size: "size",
+            material: "material",
+        }
+
+        self.product_list.append(new_product_obj)
+        self.writ_to_file()
+
+    def update(self, id, name, quantity, size, material):
+        new_product_obj = {
+            "id": id,
+            "name": name,
+            "quantity": quantity,
+            "type": "4",
+            size: "size",
+            material: "material",
+        }
+        self.delete(id)
+
+        self.product_list.append(new_product_obj)
+        self.writ_to_file()
+
+
+"""
+p1 = Product()
+p1.add("BOOK1", 25)  # name,  quantity
+
+pp2 = PerishableProduct()
+pp2.add("Milk", 30, "10 May 2026")  # expiry_date
+
+ep3 = ElectronicProduct()  #
+ep3.add("Samsung s26", 50, "2 Years")  # warranty
+
+
+cp4 = ClothingProduct()
+cp4.add("BluE ShIrt", 20, "M", "Silk")  #  size, material
+
+
+print("---")
+p1.view_all()
+print("---")
+pp2.view_all()
+print("---")
+ep3.view_all()
+print("---")
+cp4.view_all()
+print("---")
+
+"""
 
 
 p1 = Product()
 
-# p1.add("vijay", 25)
+# pp2 = PerishableProduct()
+
+# ep3 = ElectronicProduct()  
+
+cp4 = ClothingProduct()
+
+p1.view_all()
+print("---")
+
+# check id exists 
+# cp4.update("PRD-0004","Navy Blue Shirt", 30, "M", "Pure Silk")
+cp4.view_all()
+print("---")
+
+
 # p1.delete("PRD-0004")
 
-# p1.update("PRD-0001", "books", 3)
-# p1.view_all()
+p1.view_all()
+print("---")
 
-# p1.search("vIJ")
+p1.search("bo")
+
+p1.add("Pen1", 3)
+
 p1.lowstock()
+
+
+
+
+
